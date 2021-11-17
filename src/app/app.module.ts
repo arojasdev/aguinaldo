e4b0a283f4053997c52db4460ba843e6678a09e6
arojasdev <antonio.rojas.dev@gmail.com>
2021-11-17 16:20:07 -0600

    Se agrega calculo de aguinaldo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { ResultadoComponent } from './components/resultado/resultado.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     ResultadoComponent,
     FormularioComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
+  dias = 25;
+  salarioMensual = 0;
+  salarioDiario = 0;
+  aguinaldo = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarDias(event: any) {
+    this.dias = event.target.value;
+    this.calcularSalarioDiario();
+  }
+
+  calcularSalarioDiario() {
+    this.salarioDiario = this.salarioMensual / 30;
+  }
+
+  calcularAguinaldo() {
+    this.aguinaldo = this.salarioDiario * this.dias;
+    console.log(this.aguinaldo);
+    //const BMI = this.altura / Math.pow((this.altura / 100),2);
+    //this.router.navigate(['/resultado',BMI.toFixed(1)]);
   }
 
 }
