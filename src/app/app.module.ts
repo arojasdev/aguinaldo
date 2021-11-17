6121cf4936e4237605b07d7be9f4f2fb411128c5
arojasdev <antonio.rojas.dev@gmail.com>
2021-11-17 12:00:07 -0600

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ResultadoComponent } from './components/resultado/resultado.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ResultadoComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
